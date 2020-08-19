913d9fa083227e028b8b12635e57e9a7061d16bd
LTurns <lizzieturney@gmail.com>
2020-08-19 14:53:51 +0100

    moved cards into a separate component

---
+++ vue_practice/src/components/JobCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+      <b-col>
+        <b-card
+        title="This Is a Card"
+        img-src="https://picsum.photos/600/300/?images=25"
+        img-alt="job-info"
+        img-top
+        tag="article"
+        style="max-width:20rem"
+        class="mb-2"
+        >
+          <b-card-text>
+            <h1>Hello!</h1>
+          </b-card-text>
+          <b-button variant="primary">
+            Apply
+            </b-button>
+        </b-card>
+      </b-col>
+ 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JobCard'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- vue_practice/src/views/Home.vue
+++ vue_practice/src/views/Home.vue
@@ -1,64 +1,24 @@
 <template>
   <b-container>
-    <b-row align-v="center">
-      <b-col>
-        <b-card
-        title="This Is a Card"
-        img-src="https://picsum.photos/600/300/?images=25"
-        img-alt="job-info"
-        img-top
-        tag="article"
-        style="max-width:20rem"
-        class="mb-2"
-        >
-          <b-card-text>
-            <h1>Hello!</h1>
-          </b-card-text>
-          <b-button variant="primary">
-            Apply
-            </b-button>
-        </b-card>
-      </b-col>
-      <b-col>
-         <b-card
-        title="This Is a Card"
-        img-src="https://picsum.photos/600/300/?images=25"
-        img-alt="job-info"
-        img-top
-        tag="article"
-        style="max-width:20rem"
-        class="mb-2"
-        >
-          <b-card-text>
-            <h1>Hello!</h1>
-          </b-card-text>
-          <b-button variant="primary">
-            Apply
-            </b-button>
-        </b-card>
-      </b-col>
-      <b-col> <b-card
-        title="This Is a Card"
-        img-src="https://picsum.photos/600/300/?images=25"
-        img-alt="job-info"
-        img-top
-        tag="article"
-        style="max-width:20rem"
-        class="mb-2"
-        >
-          <b-card-text>
-            <h1>Hello!</h1>
-          </b-card-text>
-          <b-button variant="primary">
-            Apply
-            </b-button>
-        </b-card></b-col>
+ <b-row align-v="center">
+  <job-card></job-card>
+  <job-card></job-card>
+  <job-card></job-card>
+  <job-card></job-card>
+      
     </b-row>
   </b-container>
 </template>
 
 <script>
+
+import JobCard from '../components/JobCard.vue'
 // @ is an alias to /src
 
-export default {};
+export default {
+  name: "home",
+  components: {
+    "job-card" : JobCard
+  }
+};
 </script>
