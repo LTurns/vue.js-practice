d1a768499d64c214e97fe90f361b1c9ea250cf64
LTurns <lizzieturney@gmail.com>
2020-08-19 14:39:40 +0100

    cards for first column added

--- vue_practice/src/views/Home.vue
+++ vue_practice/src/views/Home.vue
@@ -1,18 +1,64 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <b-container>
+    <b-row align-v="center">
+      <b-col>
+        <b-card
+        title="This Is a Card"
+        img-src="https://picsum.photos/600/300/?images=25"
+        img-alt="job-info"
+        img-top
+        tag="article"
+        style="max-width:20rem"
+        class="mb-2"
+        >
+          <b-card-text>
+            <h1>Hello!</h1>
+          </b-card-text>
+          <b-button variant="primary">
+            Apply
+            </b-button>
+        </b-card>
+      </b-col>
+      <b-col>
+         <b-card
+        title="This Is a Card"
+        img-src="https://picsum.photos/600/300/?images=25"
+        img-alt="job-info"
+        img-top
+        tag="article"
+        style="max-width:20rem"
+        class="mb-2"
+        >
+          <b-card-text>
+            <h1>Hello!</h1>
+          </b-card-text>
+          <b-button variant="primary">
+            Apply
+            </b-button>
+        </b-card>
+      </b-col>
+      <b-col> <b-card
+        title="This Is a Card"
+        img-src="https://picsum.photos/600/300/?images=25"
+        img-alt="job-info"
+        img-top
+        tag="article"
+        style="max-width:20rem"
+        class="mb-2"
+        >
+          <b-card-text>
+            <h1>Hello!</h1>
+          </b-card-text>
+          <b-button variant="primary">
+            Apply
+            </b-button>
+        </b-card></b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
